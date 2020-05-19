3d9e9175a313d15ba52d7ca6beb058ee6e16dc82
Jacek <Jacek@BERLIN>
2020-05-19 07:45:09 +0200

    x

--- kurs7_sekcja9_Queries_JPQL/src/test/java/pl/jaceksysiak/hibernate/demo/JPQLTest.java
+++ kurs7_sekcja9_Queries_JPQL/src/test/java/pl/jaceksysiak/hibernate/demo/JPQLTest.java
@@ -15,6 +15,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import pl.jaceksysiak.hibernate.demo.entity.Course;
+import pl.jaceksysiak.hibernate.demo.entity.Student;
 
 
 @RunWith(SpringRunner.class)
@@ -73,5 +74,75 @@ public class JPQLTest {
 		logger.info("Select  c  From Course c where name like '%100 Steps'-> {}",resultList);
 		//[Course[Web Services in 100 Steps], Course[Spring Boot in 100 Steps]]
 	}
+	
+	@Test
+	public void jpql_courses_without_students() {
+		TypedQuery<Course> query = em.createQuery("Select c from Course c where c.students is empty", Course.class);
+		List<Course> resultList = query.getResultList();
+		logger.info("Results -> {students is empty}", resultList);
+		// [Course[Spring in 50 Steps]]
+	}
+
+	
+	@Test
+	public void jpql_courses_with_atleast_2_students() {
+		TypedQuery<Course> query = em.createQuery("Select c from Course c where size(c.students) >= 2", Course.class);
+		List<Course> resultList = query.getResultList();
+		logger.info("Results -> {student size >= 2}", resultList);
+		//[Course[JPA in 50 Steps]]
+	}
+ 
+	@Test
+	public void jpql_courses_ordered_by_students() {
+		TypedQuery<Course> query = em.createQuery("Select c from Course c order by size(c.students) desc", Course.class);
+		List<Course> resultList = query.getResultList();
+		logger.info("Results -> {students order by}", resultList);
+	}
+	
+	@Test
+	public void jpql_students_with_passports_in_a_certain_pattern() {
+		TypedQuery<Student> query = em.createQuery("Select s from Student s where s.passport.number like '%1234%'", Student.class);
+		List<Student> resultList = query.getResultList();
+		logger.info("Results -> {student, pasport like}", resultList);
+	}
+	
+	    //like
+		//BETWEEN 100 and 1000
+		//IS NULL
+		//upper, lower, trim, length
+		
+		//JOIN => Select c, s from Course c JOIN c.students s
+		//LEFT JOIN => Select c, s from Course c LEFT JOIN c.students s
+		//CROSS JOIN => Select c, s from Course c, Student s
+		//3 and 4 =>3 * 4 = 12 Rows
+		@Test
+		public void join(){
+			Query query = em.createQuery("Select c, s from Course c JOIN c.students s");
+			List<Object[]> resultList = query.getResultList();
+			logger.info("Results Size -> {JOIN}", resultList.size());
+			for(Object[] result:resultList){
+				logger.info("Course{} Student{}", result[0], result[1]);
+			}
+		}
+
+		@Test
+		public void left_join(){
+			Query query = em.createQuery("Select c, s from Course c LEFT JOIN c.students s");
+			List<Object[]> resultList = query.getResultList();
+			logger.info("Results Size -> {LEFT JOIN}", resultList.size());
+			for(Object[] result:resultList){
+				logger.info("Course{} Student{}", result[0], result[1]);
+			}
+		}
+
+		@Test
+		public void cross_join(){
+			Query query = em.createQuery("Select c, s from Course c, Student s");
+			List<Object[]> resultList = query.getResultList();
+			logger.info("Results Size -> {CROSS}", resultList.size());
+			for(Object[] result:resultList){
+				logger.info("Course{} Student{}", result[0], result[1]);
+			}
+		}
 
 }
